b2efb579d0ff0e210af574bc377c8e9db87fc43c
Joao Haskel <johaskel@gmail.com>
2020-12-07 20:20:13 -0300

    model Image

--- app/Models/PasswordReset.js
+++ app/Models/PasswordReset.js
@@ -5,21 +5,19 @@ const Model = use('Model')
 
 class PasswordReset extends Model {
   static boot() {
-     super.boot()
-     this.addHook('beforeCreate',async Model =>{
+    super.boot()
+    this.addHook('beforeCreate', async Model => {
       Model.token = await str_random(25)
       const expires_at = new Date()
       expires_at.setMinutes(expires_at.getMinutes() + 30)
       Model.expires_at = expires_at
-     })
-
+    })
   }
 
   //formata para o padraõ mysql
-  static get dates(){
-    return ['create_at','update_at','expires_at']
+  static get dates() {
+    return ['create_at', 'update_at', 'expires_at']
   }
-
 }
 
 module.exports = PasswordReset
