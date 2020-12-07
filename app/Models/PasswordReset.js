12d60e6cb72ebb9a6bfc3353a0a973051c39f138
Joao Haskel <johaskel@gmail.com>
2020-12-07 20:01:39 -0300

    ModelPasswordReset

---
+++ app/Models/PasswordReset.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class PasswordReset extends Model {
+  static boot() {
+     super.boot()
+     this.addHook('beforeCreate',async Model =>{
+      Model.token = await str_random(25)
+      const expires_at = new Date()
+      expires_at.setMinutes(expires_at.getMinutes() + 30)
+      Model.expires_at = expires_at
+     })
+
+  }
+
+  //formata para o padraõ mysql
+  static get dates(){
+    return ['create_at','update_at','expires_at']
+  }
+
+}
+
+module.exports = PasswordReset
