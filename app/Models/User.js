86282f514ca505858d94e01c853b9b3e000faafe
Joao Haskel <johaskel@gmail.com>
2020-12-07 17:30:42 -0300

    ModelUser

--- app/Models/User.js
+++ app/Models/User.js
@@ -20,6 +20,12 @@ class User extends Model {
       }
     })
   }
+
+//oculta os campos q nao quero na query
+  static get hidden(){
+  return ['password']
+  }
+
    static get traits () {
     return [
       '@provider:Adonis/Acl/HasRole',
@@ -39,6 +45,14 @@ class User extends Model {
    */
   tokens () {
     return this.hasMany('App/Models/Token')
+  }
+
+  image() {
+    return this.belongsTo('App/Models/Image')
+  }
+
+  coupons(){
+    return this.belongsToMany('App/Models/Coupon')
   }
 }
 
